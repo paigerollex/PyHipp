6c6f9f3f0be151c096e31a1ef2471eb044245e2a
AWS ParallelCluster user <ec2-user@ip-10-0-0-99.ap-southeast-1.compute.internal>
2021-11-03 01:15:07 +0000

    Lab 8 step 8

---
+++ mycheckfiles2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Number of hkl files"
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+
+echo "Number of mda files"
+find mountains -name "firings.mda" | wc -l
+
+echo "Start Times"
+head *.out -n 1
+
+echo "End Times"
+tail *.out -n 5
+
+
